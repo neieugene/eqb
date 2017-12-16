f739e2a865a27d5527869b73dfee077eb9a3d9ab
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-16 22:50:40 +0300

    hot fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
-    unless (Client.where(:idvk => request.env["omniauth.auth"].extra.raw_info.id).exists?))
+    unless (Client.where(:idvk => request.env["omniauth.auth"].extra.raw_info.id).exists?)
         client = Client.create(:idvk => request.env["omniauth.auth"].extra.raw_info.id, :first_name => request.env["omniauth.auth"].extra.raw_info.first_name, :last_name => request.env["omniauth.auth"].extra.raw_info.last_name, :photo => request.env["omniauth.auth"].extra.raw_info.photo_100)
         client.save
     end
