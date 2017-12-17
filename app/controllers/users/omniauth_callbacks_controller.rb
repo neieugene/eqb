77a7d9122cafda20546826677b8be8fd26b2eba9
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-17 20:01:44 +0300

    Debag commit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
     	client = Client.where(:idvk => request.env["omniauth.auth"].extra.raw_info.id)
     end
-    session[:current_client_id] = Client.where(:idvk => request.env["omniauth.auth"].extra.raw_info.id).first
+    session[:current_client_id] = Client.where(:idvk => request.env["omniauth.auth"].extra.raw_info.id).first.id
     redirect_to "/lab_queues"
   end
 end
