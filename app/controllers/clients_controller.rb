ad1b4d189f62b26c894e524d72f3a3db60aefa1d
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-21 00:53:48 +0300

    +3 hours

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -26,7 +26,7 @@ class ClientsController < ApplicationController
   def create
     @lab_queue = LabQueue.find(params[:lab_queue_id])
  #   unless (@lab_queue.clients.include?(Client.find(session[:current_client_id])))
-      ticket = Ticket.new(:client_id => Client.find(session[:current_client_id]).id, :lab_queue_id => params[:lab_queue_id], :time => Time.now)
+      ticket = Ticket.new(:client_id => Client.find(session[:current_client_id]).id, :lab_queue_id => params[:lab_queue_id], :time => (Time.now + 3 * 60 * 60))
       ticket.save
   #  end
     redirect_to lab_queue_path(@lab_queue)
