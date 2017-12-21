da535beb496b250c28c95617958b191d2f955f8e
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-21 17:58:08 +0300

    del ticket when del lab_queue

--- app/models/lab_queue.rb
+++ app/models/lab_queue.rb
@@ -1,5 +1,5 @@
 class LabQueue < ApplicationRecord
-	has_many :tickets
+	has_many :tickets, dependent: :destroy
 	has_many :clients, through: :tickets
   	validates :name, presence: true,
                     length: { minimum: 5 }
